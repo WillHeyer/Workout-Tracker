2fd837f0323b2cb874b660affdf675b48effe201
Will Heyer <williamhheyer@gmail.com>
2021-02-24 22:53:43 -0600

    deploying

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/MyFirstDatabase", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
