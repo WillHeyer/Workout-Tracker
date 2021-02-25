97824b42cd4952260ac7671e2ba3bfa71fa68be1
Will Heyer <williamhheyer@gmail.com>
2021-02-24 21:10:12 -0600

    everything is working except bringing the files down

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost:27107/", {
+mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
